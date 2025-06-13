72c5642fd5ee01618da852fafc2a457b30797cc0
Kevin Esensoy <kevin.esensoy@nearpod.com>
2025-06-13 13:45:58 -0400

    start in home dir

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use history::{History, HistoryEntry};
 mod screens;
 use anyhow::Result;
 use signal_hook::{consts::signal::*, iterator::Signals};
+use home::home_dir;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -32,16 +33,48 @@ fn connect(instance: InstanceInfo) -> Result<()> {
     // Run the AWS command
     let entry = HistoryEntry::new(instance.get_instance_id());
     History::save(entry)?;
-    let mut child = Command::new("aws")
-        .args([
-            "--region",
-            instance.get_region().as_ref(),
-            "ssm",
-            "start-session",
-            "--target",
-            instance.get_instance_id(),
-        ])
-        .spawn()?;
+
+    // Try AWS-StartInteractiveCommand first to start in home directory
+    let mut command = Command::new("aws");
+    command.args([
+        "--region",
+        instance.get_region().as_ref(),
+        "ssm",
+        "start-session",
+        "--target",
+        instance.get_instance_id(),
+        "--document-name",
+        "AWS-StartInteractiveCommand",
+        "--parameters",
+        r#"command="cd ~ && exec bash -l""#,
+    ]);
+
+    // Set local working directory to home directory if possible
+    if let Some(home_dir) = home_dir() {
+        command.current_dir(home_dir);
+    }
+
+    let mut child = match command.spawn() {
+        Ok(child) => child,
+        Err(_) => {
+            println!("AWS-StartInteractiveCommand failed, trying standard start-session");
+            let mut fallback_command = Command::new("aws");
+            fallback_command.args([
+                "--region",
+                instance.get_region().as_ref(),
+                "ssm",
+                "start-session",
+                "--target",
+                instance.get_instance_id(),
+            ]);
+
+            if let Some(home_dir) = home_dir() {
+                fallback_command.current_dir(home_dir);
+            }
+
+            fallback_command.spawn()?
+        }
+    };
 
     // Catch SIGINT, SIGSTP signal and do nothing
     // So that actually ctrl+c / ctrl+z works on the aws ssm session instead of killing / stopping us
